f8bcfe294c05787c2ba3ab0e679a64966ab53908
Flyan <guilhermeflyan.22@gmail.com>
2025-11-15 20:05:48 -0300

    Try deploy again

--- src/frontend/app/dashboard/file/page.tsx
+++ src/frontend/app/dashboard/file/page.tsx
@@ -1,5 +1,12 @@
 import ClientDashboardFile from "@/app/components/DashBoardFile";
+import Spinner from "@/app/components/Spinner";
+import { Suspense } from "react";
+
 
 export default function Page() {
-    return <ClientDashboardFile/>
+    return <>
+    <Suspense fallback={<Spinner/>}>
+        <ClientDashboardFile/>
+    </Suspense>
+    </>
 }
