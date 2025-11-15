55ea5cb47c7e3759660319f39b3ab147b8fb540e
Flyan <guilhermeflyan.22@gmail.com>
2025-11-15 15:19:20 -0300

    Add CORS

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from ai import *
+
 app = FastAPI()
+
+# Configuração CORS
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def root():
     return {"message": "It's working!"}
